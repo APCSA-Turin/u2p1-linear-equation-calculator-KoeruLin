9b7f173a90da395359d49c1c63ffb9ca102a9dd7
KoeruLin <167535514+KoeruLin@users.noreply.github.com>
2024-10-16 13:30:29 +0000

    commit

--- src/main/java/com/example/project/LinearCalculator.java
+++ src/main/java/com/example/project/LinearCalculator.java
@@ -1,37 +1,75 @@
 package com.example.project;
-public class LinearCalculator{
+import java.text.DecimalFormat;
+
+public class LinearCalculator
+{
     //INSTANCE VARIABLES 
     //4 INTEGER variables (name them: x1,x2,y1,y2) 
 
-
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
 
     //CONSTRUCTOR
     //1 constructor with 2 String parameters. Each parameter represents a coordinate. 
     //For example, "(1,2)" and "(3,4)" would be two parameter values 
     //You will have to parse the string into 4 integers, representing the 2 points.
-    public LinearCalculator(){ // <--add 2 string parameters to this constructor
-     
+    public LinearCalculator(String firstCoordinate, String secondCoordinate)
+    {
+        x1 = Integer.parseInt(firstCoordinate.substring(1, 2));
+        y1 = Integer.parseInt(firstCoordinate.substring(3, 4));
+        x2 = Integer.parseInt(secondCoordinate.substring(1, 2));
+        y2 = Integer.parseInt(secondCoordinate.substring(3, 4));
+
     }
 
 
 
     //METHODS
     //getters and setters for the 4 instance variables (8 methods total) 
-    public int getX1(){return 0;}
-    public int getY1(){return 0;}
-    public int getX2(){return 0;}
-    public int getY2(){return 0;}
-    public void setX1(){}
-    public void setY1(){}
-    public void setX2(){}
-    public void setY2(){}
+    public int getX1()
+    {
+        return x1;
+    }
+    public int getY1()
+    {
+        return y1;
+    }
+    public int getX2()
+    {
+        return x2;
+    }
+    public int getY2()
+    {
+        return y2;
+    }
+    public void setX1(int x1)
+    {
+        this.x1 = x1;
+    }
+    public void setY1(int y1)
+    {
+        this.y1 = y1;
+    }
+    public void setX2(int x2)
+    {
+        this.x2 = x2;
+    }
+    public void setY2(int y2)
+    {
+        this.y2 = y2;
+    }
 
 
     //distance() -> returns a double. 
     //calculates the distance between the two points to the nearest HUNDREDTH and returns the value.
-    public double distance(){
-        return 0.0;
-
+    public double distance()
+    {
+        DecimalFormat format = new DecimalFormat("#.##");
+        String stringDistance = format.format((x2 - x1) / (y2 - y1));
+        return Double.parseDouble(stringDistance);
+    }
     //yInt() -> returns a double.
     //calculates the y intercept of the equation and returns the value to the nearest HUNDREDTH
     //if y-int if undefined, should return -999.99
@@ -65,8 +103,8 @@ public class LinearCalculator{
     //this method is tested but you can also call it in your main method if gradle tests are 
     //not working. 
     public String printInfo(){
-        String str = "The two points are: (" + /*insert var here*/ + "," +/*insert var here*/  + ")";
-        str += " and " + "(" + /*insert var here*/ + "," + /*insert var here*/ + ")";
+        String str = "The two points are: (" + x1 + "," + y1 + ")";
+        str += " and " + "(" + x2 + "," + y2 + ")";
         str += "\nThe equation of the line between these points is: " ;
         str += "\nThe slope of this line is: ";
         str += "\nThe y-intercept of the line is: ";
